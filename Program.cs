33ff3ffd9eee7c1356943bcd4148891b84324cdc
Thato Motaung <39223762+ThisThato@users.noreply.github.com>
2021-07-07 07:38:13 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿    using System;
+
+namespace AdaptUniversity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
